8953dd7842976082a0b70bb7a6e2617b1ac52646
NoOne6879 <20108791@mail.wit.ie>
2026-09-15 11:54:19 +0100

    Lab 1 small fix to center the cards

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -3,7 +3,7 @@ import Task from './components/Task';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <h1>Tasky</h1>
       <Task title="Dishes" deadline="Today" description="Empty dishwasher" />
       <Task title="Laundry" deadline="Tomorrow" description="Fold laundry and put it away" />
